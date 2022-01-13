34506a66758a7543b11ba186c670eb2b4b0b4985
davidschubert <github@davidschubert.com>
2022-01-13 16:52:20 +0100

    update data

--- layouts/error.vue
+++ layouts/error.vue
@@ -4,11 +4,11 @@
       <template v-if="error.statusCode === 404">
         <h1 class="title">{{ error.message || 'Page Not Found' }}</h1>
         <h2 class="subtitle">404</h2>
-        <p class="mb-4">Looks like you've followed a broken link or entered a URL that doesn't exist on this site.</p>
+        <p class="mb-4">Offenbar sind Sie einem defekten Link gefolgt oder haben eine URL eingegeben, die auf dieser Website nicht existiert.</p>
       </template>
       <template v-else>
-        <h1 class="title">An error occurred</h1>
-        <p class="mb-4">Looks like you've followed a broken link or entered a URL that doesn't exist on this site.</p>
+        <h1 class="title">Ein Fehler ist aufgetreten</h1>
+        <p class="mb-4">Offenbar sind Sie einem defekten Link gefolgt oder haben eine URL eingegeben, die auf dieser Website nicht existiert.</p>
       </template>
       <nav class="mt-12" aria-label="go to home">
         <router-back to="/" class="block"/>
