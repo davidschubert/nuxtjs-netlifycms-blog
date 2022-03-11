a84670f547070a00840bbf679baf26e54e83394f
davidschubert <github@davidschubert.com>
2022-03-11 13:28:28 +0100

    added two apis to instagram site

--- pages/instagram/index.vue
+++ pages/instagram/index.vue
@@ -7,10 +7,38 @@
       <h1 class="title">Instagram</h1>
       <h2>Hier spiele ich mit der API von Instagram rum</h2>
       <p>Das wird spannend!</p>
+      <h2>IP API:</h2>
+      <p><b>Meine IP-Adresse:</b> {{ ip }}</p>
+      <br />
+      <h2>Bored API:</h2>
+      <p>
+        <b>activity:</b> {{ bored.activity }}<br />
+        <b>type:</b> {{ bored.type }}<br />
+        <b>participants:</b> {{ bored.participants }}<br />
+        <b>price:</b> {{ bored.price }}<br />
+        <b>link:</b> {{ bored.link }}<br />
+        <b>key:</b> {{ bored.key }}<br />
+        <b>accessibility:</b> {{ bored.accessibility }}<br />
+      </p>
     </section>
   </main>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+
+// API Endpoints
+let apiIp = 'http://icanhazip.com'
+let apiBored = 'http://www.boredapi.com/api/activity/'
+
+export default {
+  async asyncData() {
+    const dataIp = await axios.get(apiIp)
+    const dataBored = await axios.get(apiBored)
+    return {
+      ip: dataIp.data,
+      bored: dataBored.data,
+    }
+  },
+}
 </script>
